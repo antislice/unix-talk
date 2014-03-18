06ae8085e894f0c918b2c1686f9957420286bccb
gandrews <gandrews@guidewire.com>
2014-03-18 15:08:34 -0700

    add --sort

--- birite.rb
+++ birite.rb
@@ -2,12 +2,24 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'optparse'
 
 def icecream_list
 
 	doc = Nokogiri::HTML(open('http://biritecreamery.com/icecream'))
-	doc.at('h3:contains("scoops")').next_element.css('li').map {|flavor_li| flavor_li.inner_text}
-
+	flavors = doc.at('h3:contains("scoops")').next_element.css('li').map {|flavor_li| flavor_li.inner_text}
+	flavors.sort! if $options[:sort]
+	flavors
 end
+
+$options = {}
+
+OptionParser.new do |opts|
+	opts.banner = 'Usage: birite [-s] <menu item>'
+
+	opts.on('-s', '--sort', "Sort flavors") do |s|
+		$options[:sort] = s
+	end
+end.parse!
 
 puts icecream_list
